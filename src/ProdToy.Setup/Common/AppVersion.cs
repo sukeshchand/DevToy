4561426908f787b41aed34d23983dbb4ab19c478
Sukesh Chand <sukesh.chand@3bits.se>
2026-04-13 17:29:05 +0200

    chore: Bump to v1.0.289

--- src/ProdToy.Setup/Common/AppVersion.cs
+++ src/ProdToy.Setup/Common/AppVersion.cs
@@ -5,5 +5,5 @@ namespace ProdToy.Setup;
 /// </summary>
 static class AppVersion
 {
-    public const string Current = "1.0.288";
+    public const string Current = "1.0.289";
 }
--- src/ProdToy.Win/Core/AppVersion.cs
+++ src/ProdToy.Win/Core/AppVersion.cs
@@ -2,5 +2,5 @@ namespace ProdToy;
 
 static class AppVersion
 {
-    public const string Current = "1.0.288";
+    public const string Current = "1.0.289";
 }
